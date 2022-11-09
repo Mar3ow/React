59c5910c4349257330b9632abd7e8d0a76f6158b
Mar3ow <mar3ow@gmail.com>
2022-11-09 21:18:34 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TodoList from "./Todo/TodoList"
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h1>Раработка сайта на React :</h1>
+      <TodoList></TodoList>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Todo/TodoList.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function TodoList(props) {
+    return (
+        <h3>Добавление компонента</h3>
+    )
+}
